b843165b1a0a40c28de480d9e9bc7e21bcca83f2
Abilash Sivasith <abi.sivasith.world@gmail.com>
2025-02-02 03:03:46 +1300

    did some work but styling not working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-import {Route, Routes} from "react-router-dom"
-import './App.css'
-import HomePage from "./pages/HomePage"
-
 const Page1 = () => <h1 className="text-2xl">This is Page 1</h1>;
 const Page2 = () => <h1 className="text-2xl">This is Page 2</h1>;
 
+import './index.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <>
-    <p>Hello from apps.jsx</p>
-  </>
-  )
+    <>
+      <p className='text-5xl text-red-400'>Hello from apps.jsx</p>
+    </>
+  );
 }
  
 export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
