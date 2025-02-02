28176e326b016e5d4efebbf9a0d8fececdfa9f34
Abilash Sivasith <abi.sivasith.world@gmail.com>
2025-02-02 15:01:47 +1300

    working daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,13 @@ function App() {
   return (
     <>
       <p className='text-5xl text-red-500'>Hello from apps.jsx</p>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </>
   );
 }
