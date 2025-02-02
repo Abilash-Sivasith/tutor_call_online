7d6ee1b9ffc143bcd0e0e7aabead2acbddb55f03
Abilash Sivasith <abi.sivasith.world@gmail.com>
2025-02-02 15:43:11 +1300

    homepage has buttons but visually looks bad

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,18 @@
 // const Page1 = () => <h1 className="text-2xl">This is Page 1</h1>;
 // const Page2 = () => <h1 className="text-2xl">This is Page 2</h1>;
 
-// import './index.css'
+import {Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage"
+
 
 function App() {
   return (
-    <>
-      <p className='text-5xl text-red-500'>Hello from apps.jsx</p>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+        </Routes>
+    </div>
+
   );
 }
  
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,16 +2,15 @@ const homePage = () => {
     return (
         <div className="flex justify-center items-center h-screen">
           <div className="flex gap-4">
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 ">
               Button 1
             </button>
-            <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600">
+            <button className="px-10 py-5 bg-green-500 text-white rounded-lg hover:bg-green-600">
               Button 2
             </button>
           </div>
         </div>
       );
  }
- 
  
  export default homePage;
