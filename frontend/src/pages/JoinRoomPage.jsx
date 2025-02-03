a7d9780e82449928505867ed11432a79e502034f
Abilash Sivasith <abi.sivasith.world@gmail.com>
2025-02-04 00:54:33 +1300

    added join room page text boxes

--- frontend/src/pages/JoinRoomPage.jsx
+++ frontend/src/pages/JoinRoomPage.jsx
@@ -1,9 +1,30 @@
-const JoinRoomPage = () => {
-    return (
-        <div>
-            <h1 className="text-2xl">Welcome to JoinRoomPage</h1>
-        </div>
-    )
-};
+import { useNavigate } from "react-router-dom";
 
-export default JoinRoomPage;
+const JoinRoomPage = () => {
+    const navigate = useNavigate();
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex flex-col gap-4">
+          <input 
+            type="text" 
+            placeholder="Room Code" 
+            className="input input-bordered w-full max-w-xs"
+          />
+          <input 
+            type="text" 
+            placeholder="Username" 
+            className="input input-bordered w-full max-w-xs"
+          />
+            <button 
+            className="px-20 py-5 bg-green-500 text-white rounded-lg hover:bg-green-600" 
+            onClick={() => navigate("/")}
+          >
+            Join Room
+          </button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default JoinRoomPage;
+  
