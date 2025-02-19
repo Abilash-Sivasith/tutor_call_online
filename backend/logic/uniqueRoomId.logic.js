fcd628117222c04bb499e1024bd35fbcff0bb23a
Abilash Sivasith <abi.sivasith.world@gmail.com>
2025-02-20 00:22:56 +1300

    changed room code to just numbers

--- backend/logic/uniqueRoomId.logic.js
+++ backend/logic/uniqueRoomId.logic.js
@@ -1,11 +1,11 @@
 
 export const uniqueRoomIdGenerator = () => {
     let generateRoomId = ""
-    let alphabetAndNumbers = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('');
-    for (let i = 0; i < 6; i++) {
+    let alphabetAndNumbers = '0123456789'.split('');
+    for (let i = 0; i < 5; i++) {
         let randomCharacterOrNumber = alphabetAndNumbers[Math.floor(Math.random() * alphabetAndNumbers.length)];
         generateRoomId += randomCharacterOrNumber
     }
     // console.log("unique roomId --> ", generateRoomId)
-    return generateRoomId;
+    return generateRoomId.toUpperCase();
 }
