649c09337be18e159579cd82b80a20cadd26982f
asi160 <asi160@uclive.ac.nz>
2025-05-11 16:39:02 +1200

    trying to implement waitlist

--- frontend/src/pages/common/components/waitlist.jsx
+++ frontend/src/pages/common/components/waitlist.jsx
@@ -21,7 +21,7 @@
           throw new Error("something went wrong fetching the waitlist");
         }
     
-        // const data = await response.json();
+        const data = await response.json();
         setWaitlist(response.data);
       } catch (error) {
         setErrorFlag(true);
@@ -40,11 +40,11 @@
       })
     }
 
-    /** 
+    
     if (errorFlag) {
       return (
         <div>
-          <div style={{color = "red"}}>{errorMessage}</div>
+          <div>{errorMessage}</div>
         </div>
       )
     } else {
@@ -63,111 +63,6 @@
           </table>
         </div>
       )
-    }
-    */
-
-
-
-
-    return (
-      <div>
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Question</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>4</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>5</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>6</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>7</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>8</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>9</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>10</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>11</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>12</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>13</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>14</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>15</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-            <tr>
-              <th>16</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-            </tr>
-          </tbody>
-      </table>
-    </div>
-    );
-  }
-
-  export default WaitlistComponent;
+    };
+  }  
+export default WaitlistComponent;
